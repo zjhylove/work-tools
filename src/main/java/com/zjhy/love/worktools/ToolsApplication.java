6dd94c5e1fb6e7fc1f6eef6f1c37ae361aa4af51
zhengjun <zhengjun@sporttery.cn>
2024-12-23 13:16:29 +0800

    调整目录结构

--- src/main/java/com/zjhy/love/worktools/ToolsApplication.java
+++ src/main/java/com/zjhy/love/worktools/ToolsApplication.java
@@ -3,10 +3,12 @@ package com.zjhy.love.worktools;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import org.kordamp.bootstrapfx.BootstrapFX;
 
 import java.io.IOException;
+import java.util.Objects;
 
 /**
  * @author zhengjun
@@ -18,8 +20,17 @@ public class ToolsApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(ToolsApplication.class.getResource("/view/layout-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 600);
         // 添加BootstrapFX样式表
-        scene.getStylesheets().add(BootstrapFX.bootstrapFXStylesheet());
-        stage.setTitle("Work Tools");
+        scene.getStylesheets().addAll(
+            BootstrapFX.bootstrapFXStylesheet(),
+            Objects.requireNonNull(getClass().getResource("/css/application.css")).toExternalForm()
+        );
+        
+        // 设置标题样式
+        stage.setTitle("✨ Work Tools ⚡");
+        
+        // 设置应用图标
+        stage.getIcons().add(new Image(Objects.requireNonNull(ToolsApplication.class.getResourceAsStream("/images/tools-icon.png"))));
+        
         stage.setScene(scene);
         stage.show();
     }
